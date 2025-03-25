99cca03f7a6453deab1b570b00b25d0959bf7640
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-03-25 08:46:54 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ export const App = () => {
           subtitle="Нужно открыть брокерский счёт. Затем попробуйте снова, воспользовавшись формой для перевода активов"
           onClick={() => {
             window.gtag('event', '4532_bs_var3');
-            window.location.replace('alfabank://investments/open_brokerage_account');
+            window.location.replace('https://alfa-mobile.alfabank.ru/mobile/goto/open_brokerage_account');
           }}
         />
       );
